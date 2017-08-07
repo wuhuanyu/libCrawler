e54b375c40a6c0195ea6182d3c3b3180013883d5
wuhuanyu <1790949749@qq.com>
2017-08-07 18:49:34 +0800

    reuters

--- stacklib/spiders/reuters.py
+++ stacklib/spiders/reuters.py
@@ -2,20 +2,55 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from time import time
+from ..items import ReutersItem
 
+
+def check(arr):
+    if type(arr)=='list':
+        return len(arr)
+    return arr is not None
 
 class ReutersSpider(CrawlSpider):
     name = 'reuters'
     allowed_domains = ['reuters.com']
     start_urls = ['http://reuters.com/']
+    source = 'reuters'
+    crawled_at = int(round(time() * 1000))
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    base_url = start_urls[0]
 
-    def parse_item(self, response):
-        i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+    url_tags = {
+        'finance': 'business',
+        'politics': 'politics',
+        'news/technology': 'tech',
+        'commentary': 'commentary',
+        'places/china': 'china',
+        'news/us': 'politics',
+
+    }
+
+    def start_request(self):
+        for url, tag in self.url_tags.iteritems():
+            yield scrapy.Request(self.base_url + url, callback=self.parse_list, meta={'tag': tag})
+
+    def parse_list(self, res):
+        tag = res.meta['tag']
+        main_panel = res.css('.column1.gridPanel.grid8')
+        if not check(main_panel):
+            raise Exception('main pannel not found!')
+        
+        top_sty = main_panel.xpath('.//div[@id="topStory"]')
+        
+        top_sty_url = top_sty.xpath('.//div[@class="photo"]/a/@href').extract_first()
+
+
+        
+
+        
+        
+
+
+    def parse_art(self,res):
+        pass
+    
