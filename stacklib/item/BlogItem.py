35589731bf70cc87bc686fc6403dfb229373a74e
wuhuanyu <1790949749@qq.com>
2017-08-10 21:38:44 +0800

    mb begin

--- stacklib/item/BlogItem.py
+++ stacklib/item/BlogItem.py
@@ -6,3 +6,4 @@ class BlogItem(News):
 
 class Medium(BlogItem):
     pass
+
---
+++ stacklib/item/BookItem.py
@@ -0,0 +1,21 @@
+import scrapy
+
+from w3lib.html import remove_tags
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from NewsItem import BaseItem
+
+
+class BookItem(BaseItem):
+    source = scrapy.Field(output_processor=TakeFirst())
+
+    related_url = scrapy.Field()
+
+    author = scrapy.Field(input_processor = MapCompose(remove_tags))
+    summary = scrapy.Field(input_processor=MapCompose(remove_tags))
+
+    
+
+
+
+
+    
