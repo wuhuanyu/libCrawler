7c21c282b958ef8e2e12214f470e1378312930ae
wuhuanyu <1790949749@qq.com>
2017-08-12 12:24:14 +0800

    mbook

--- stacklib/item/BookItem.py
+++ stacklib/item/BookItem.py
@@ -5,25 +5,28 @@ from scrapy.loader.processors import Join, MapCompose, TakeFirst
 from NewsItem import BaseItem
 
 
-class BookItem(BaseItem):
+class Book(BaseItem):
     source = scrapy.Field(output_processor=TakeFirst())
 
-    title = scrapy.Field(input_processor=MapCompose(remove_tags),output_processor=TakeFirst())
-
+    title = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
 
     review_urls = scrapy.Field()
 
-    author = scrapy.Field(input_processor = MapCompose(remove_tags))
+    author = scrapy.Field(input_processor=MapCompose(remove_tags))
 
     summary = scrapy.Field(input_processor=MapCompose(remove_tags))
 
 
+class BookReivew(BaseItem):
+    title = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    text = scrapy.Field(input_processor=MapCompose(remove_tags))
+    comments = scrapy.Field()
 
 
+class Comment(scrapy.Item):
+    author = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
 
-    
-
-
-
-
-    
+    text = scrapy.Field(input_processor=MapCompose(remove_tags))
--- stacklib/spiders/mbook.py
+++ stacklib/spiders/mbook.py
@@ -2,7 +2,9 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-from stacklib.item.BookItem import BookItem as BItem
+from stacklib.item.BookItem import Book as BI
+from stacklib.item.BookItem import BookReivew as BR
+from stacklib.item.BookItem import Comment as CO
 
 from time import time
 
@@ -11,7 +13,7 @@ from scrapy import Request as Re
 
 
 class MillionBookSpider(CrawlSpider):
-    crawled_at = int(round(time()*1000))
+    crawled_at = int(round(time() * 1000))
     name = 'mbook'
     # allowed_domains = ['themillions.com']
     start_urls = ['http://themillions.com']
@@ -36,16 +38,64 @@ class MillionBookSpider(CrawlSpider):
     def parse_book(self, res):
         url = res.url
 
-        main = res.css('.main-content')
+        main = res.css('#main-content')
 
-        bi = scrapy.loader.ItemLoader(item=BItem(), selector=main)
+        bi = scrapy.loader.ItemLoader(item=BI(), selector=main)
         bi.add_value('source', self.source)
         bi.add_value('crawled_at', self.crawled_at)
         bi.add_value('url', url)
 
         bi.add_xpath('summary', './/div[@class="article"]//p/text()')
-        bi.add_xpath('image_urls', './/div[@id="br-book-detail"]//img/@href')
+        bi.add_xpath('image_urls', './/div[@id="br-book-detail"]//img/@src')
         bi.add_xpath('title', './/div[@id="br-details"]/h1/a/text()')
-        bi.add_xpath('author', './/h4/text()')
-        bi.add_xpath('review_urls', './/div[@class="article"]/h2/a/@href')
-        return bi.load_item()
+        bi.add_xpath('author', './/div[@class="br-details"]//h4/text()')
+
+        img_ = main.xpath('.//div[@class="article"]/h2/a/@href').extract()
+
+        related_urls = filter(lambda x: not ('comments' in x), img_)
+
+        bi.add_value('review_urls', related_urls)
+
+        for url in related_urls:
+            yield Re(url, callback=self.parse_book_reivew)
+
+        # bi.add_xpath('review_urls', './/div[@class="article"]/h1/a/@href')
+        yield bi.load_item()
+
+
+# http://www.themillions.com/2013/01/the-testament-of-mary-by-colm-toibin.html
+    def parse_book_reivew(self, res):
+        crawled_at = int(round(time()) * 1000)
+        source = 'mbookrev'
+
+        art = res.css('#main-content').xpath('./div[@id="article"]')
+        cat = art.xpath('./h2/a[@rel]/text()').extract_first()
+
+        if not (cat.lower() == 'reviews'):
+            return
+
+        bi = scrapy.loader.ItemLoader(item=BR(), selector=art)
+        bi.add_value('crawled_at', crawled_at)
+        bi.add_value('source', source)
+        bi.add_css('title', 'h2+h3::text')
+        bi.add_css('text', '.article-content>p::text')
+
+        comments = []
+
+        com_sel = res.css('.commentlist')
+
+        commentli = com_sel.xpath('.//li[re:test(@id,"^comment-\d*$")]')
+
+        for c in commentli:
+            comment = CO()
+            comment['author'] = c.xpath('.//span[@class="author"]/text()').extract_first()
+            # author= c.xpath('.//span[@class="author"]').extract_first()
+
+            comment['text'] = c.css('.commenttext>p::text').extract()
+            comments.append(comment)
+
+        bi.add_value('comments', comments)
+
+        return  bi.load_item()
+
+        # commentText = com_sel.xpath('.//li[re:test(@id,"^comment-\d*$")]//div[@class="commenttext"]/p/text()').extract()
